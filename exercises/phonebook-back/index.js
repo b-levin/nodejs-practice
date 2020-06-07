5e85094b3c027707b4465f6b0aae30e96a59831b
blevin <windrider417@gmail.com>
2020-06-07 16:54:36 -0400

    added phonebook deployment

--- exercises/phonebook-back/index.js
+++ exercises/phonebook-back/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 
 let persons = [
     {
@@ -95,7 +98,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
