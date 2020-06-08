443156852b38ca36f16aa3cd3ab16c8afbd67f32
blevin <windrider417@gmail.com>
2020-06-08 09:38:17 -0400

    minor backend updates

--- exercises/phonebook-back/index.js
+++ exercises/phonebook-back/index.js
@@ -4,8 +4,9 @@ var morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
-app.use(express.static('build'))
 app.use(cors())
+app.use(express.static('build'))
+
 
 let persons = [
     {
