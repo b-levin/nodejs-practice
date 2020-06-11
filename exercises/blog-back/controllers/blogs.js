7e1dd336676e3d7e6b90aae9e3f62858c3c0fd95
blevin <windrider417@gmail.com>
2020-06-11 16:55:46 -0400

    added login

--- exercises/blog-back/controllers/blogs.js
+++ exercises/blog-back/controllers/blogs.js
@@ -1,24 +1,47 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+//gets the token form
+const getTokenForm = req => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 //gets all of the blogs stores in the database
 blogsRouter.get('/', async (req, res) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({}).populate('user', { username: 1, name: 1 })
+
     res.json(blogs.map(blog => blog.toJSON()))
 })
 
 //adds a blog to the databage
 blogsRouter.post('/', async (req, res) => {
     const body = req.body
+    const token = getTokenForm(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     res.json(savedBlog.toJSON())
 })
 
