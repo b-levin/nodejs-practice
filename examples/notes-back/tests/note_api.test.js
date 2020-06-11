603351129c42e555c5afbe41f0d0f7b00c54ee78
blevin <windrider417@gmail.com>
2020-06-11 11:54:56 -0400

    added users

--- examples/notes-back/tests/note_api.test.js
+++ examples/notes-back/tests/note_api.test.js
@@ -3,8 +3,11 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 const Note = require('../models/note')
+const { base } = require('../models/user')
 
 beforeEach(async () => {
     await Note.deleteMany({})
@@ -113,6 +116,39 @@ describe('deletion of a note', () => {
         const contents = notesAtEnd.map(r => r.content)
 
         expect(contents).not.toContain(noteToDelete.content)
+    })
+})
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('secret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
     })
 })
 
