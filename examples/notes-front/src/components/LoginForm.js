e0cf176d0cd2af7c393359d2a057e75120a2a7a8
blevin <windrider417@gmail.com>
2020-06-13 16:47:02 -0400

    minor updates

--- examples/notes-front/src/components/LoginForm.js
+++ examples/notes-front/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
     handleSubmit,
@@ -33,6 +34,14 @@ const LoginForm = ({
             </form>
         </div>
     )
+}
+
+LoginForm.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    handleUsernameChange: PropTypes.func.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+    username: PropTypes.func.isRequired,
+    password: PropTypes.func.isRequired
 }
 
 export default LoginForm
